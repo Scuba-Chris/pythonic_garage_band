a2786871a853a186b3ea235142a85f409d076eda
Christopher Ceder <cceder@Christophers-MacBook-Air.local>
2019-12-05 21:37:46 -0800

    still working on getting all functionality working

--- band.py
+++ band.py
@@ -1,28 +1,34 @@
+
+
 class Bands:
     
     all_bands = []
-
+   
     def __init__(self, band_name, members=[]):
         self.band_name = band_name
         self.members = members
         self.__class__.all_bands.append(self)
 
+    def __str__(self):
+        return f'the band name is {self.band_name}'
+        
 class Musician:
 
     def __init__(self, name, insturment):
         self.name = name
         self.insturment = insturment
 
-class Singer(Musician):
-    def __init__(self, name):
-        super().__init__(name, 'singer')
-
 class Guitarist(Musician):
     def __init__(self, name):
         super().__init__(name, 'guitarist')
+
+class Singer(Musician):
+    def __init__(self, name):
+        super().__init__(name, 'singer')
 
 class Drummer(Musician):
     def __init__(self, name):
         super().__init__(name, 'drummer')
 
-
+    def i_play(self):
+        print(self.name)
--- test_band.py
+++ test_band.py
@@ -1,3 +1,15 @@
 import pytest
 
-from band import Bands, Musician, 
+from band import Bands, Musician, Singer, Guitarist, Drummer
+
+def test_member_one():
+    test_drummer = Drummer('Ashlyn')
+    assert isinstance(test_drummer, Drummer)
+
+def test_member_two():
+    test_singer = Singer('Kathy')
+    assert isinstance(test_singer, Singer)
+
+def test_member_three():
+    test_guitarist = Guitarist('Sean')
+    assert isinstance(test_guitarist, Guitarist)
